2ab8594b24ed5dbca557bd2aa78a1a6dadd51331
ducmc4002-del <ducmc4002@gmail.com>
2025-10-22 16:43:28 +0700

    2

---
+++ AuthService/Migrations/20251022070438_AddAvatar.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAvatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AvataThumbnail",
+                table: "Users",
+                newName: "AvatarThumbnail");
+
+            migrationBuilder.RenameColumn(
+                name: "Avata",
+                table: "Users",
+                newName: "Avatar");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AvatarThumbnail",
+                table: "Users",
+                newName: "AvataThumbnail");
+
+            migrationBuilder.RenameColumn(
+                name: "Avatar",
+                table: "Users",
+                newName: "Avata");
+        }
+    }
+}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using UserService.Data;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,7 +37,38 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    // Thêm ??nh ngh?a b?o m?t (Security Definition)
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http, 
+        Scheme = "Bearer", // Tên scheme là Bearer
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header, // Token n?m trong Header
+        Description = "Enter 'Bearer' + your token "
+    });
+
+    // Thêm yêu c?u b?o m?t (Security Requirement)
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
