c276a692891c4b827b72cfba5b8416e8c5a59bca
M1nkDuk <duccmgch221016@fpt.edu.vn>
2025-11-19 22:13:26 +0700

    Mirgtaion addchapterID for ReadingHistory

---
+++ UserService/Migrations/20251119150554_AddChapterIdToHistory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChapterIdToHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChapterId",
+                table: "ReadingHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChapterId",
+                table: "ReadingHistories");
+        }
+    }
+}
